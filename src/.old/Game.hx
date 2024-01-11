b5641a875a801e858a621072b307900d8e179a07
HiddenWisent <asdowa447@gmail.com>
2024-01-11 17:24:12 +0100

    Separated old tests

---
+++ src/.old/Game.hx
@@ -0,0 +1,40 @@
+		// var t1 = Tile.fromColor(0xFF0000, 100, 100);
+		// var t2 = Tile.fromColor(0x00FF00, 100, 100);
+		// var t3 = Tile.fromColor(0x0000FF, 100, 100);
+		// anim = new Anim([t1, t2, t3], s2d);
+		// anim.x = s2d.width * 0.5 - 50;
+		// anim.y = s2d.height * 0.5 - 50;
+		// anim.speed = 9;
+		// anim.pause = true;
+
+		// var interaction = new h2d.Interactive(100, 100, anim);
+		// interaction.onOver = function(event:hxd.Event) {
+		// 	anim.filter = new Glow();
+		// }
+		// interaction.onOut = function(event:hxd.Event) {
+		// 	anim.filter = null;
+		// }
+		// interaction.onClick = function(event:hxd.Event) {
+		// 	anim.pause = !anim.pause;
+		// }
+		// var healthBar = new ProgressBar(s2d, 0, 10, 100, 20);
+		// var hpInt = new h2d.Interactive(100, 20, healthBar);
+		// hpInt.onClick = function(event:hxd.Event) {
+		// 	healthBar.valueChange(4);
+		// }
+
+		// var tile = Tile.fromColor(0xFFFFFF, 50, 50);
+		// var level = new TileGroup(tile, s2d);
+		// level.x = s2d.width * 0.5;
+		// level.y = s2d.width * 0.5;
+
+		// level.add(0,0,tile);
+		// level.add(50,0,tile);
+		// level.add(-50,0,tile);
+		// level.add(100,0,tile);
+		// level.add(-100,-100,tile);
+		// level.add(-100,0,tile);
+		// level.add(50,-50,tile);
+		// level.add(100,-50,tile);
+		// level.add(100,-100,tile);
+		// level.add(-100,-50,tile);
